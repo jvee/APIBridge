ab67827777b8654deaacebd81ab01951d7e862a4
Ivan Zasimoff <jvee99@gmail.com>
2014-09-11 01:48:53 +0400

    Тесты для модуля api-node

---
+++ test/spec-api-node.js
@@ -0,0 +1,64 @@
+var assert = require('assert'),
+	APINode = require('../lib/api-node'),
+	data = require('./data'),
+	apiNodeDecl = data.decl_1_normalized['.locations.search'],
+	apiNodePath = '.locations.search';
+	shadowTreeStub = {
+		splitPath: function () {
+			return ['.', '.locations', '.locations.search'];
+		},
+		getNode: function (nodePath) {
+			return this[nodePath];
+		},
+		'.locations': data.decl_1_normalized['.locations'],
+		'.': data.decl_1_normalized['.']
+	};
+
+describe('APINode', function () {
+
+	beforeEach(function () {
+		apiNode = new APINode(apiNodeDecl, apiNodePath, shadowTreeStub);
+	});
+
+	it('should construct object', function () {
+		assert.equal(apiNode.name, 'search');
+		assert.equal(apiNode.nodeType, 'endpoints');
+		assert.equal(apiNode.path, '.locations.search');
+		assert.deepEqual(apiNode.parents, ['.', '.locations']);
+	});
+
+	describe('APINode#copyOptions()', function () {
+		it('should copy declared options and ignore some from defaults', function () {
+			assert.equal(apiNode.options.name, undefined);
+			assert.equal(apiNode.options.nodeType, undefined);
+			assert.equal(apiNode.options.url, 'locations/search');
+		});
+	});
+
+	describe('APINode#getParent()', function () {
+		it('should return parent node without arguments', function () {
+			assert.equal(apiNode.getParent(), shadowTreeStub['.locations']);
+		});
+
+		it('should return node from path in arguments', function () {
+			assert.equal(apiNode.getParent('.'), shadowTreeStub['.']);
+		});
+
+		it('should return undefined with wrong argument', function () {
+			assert.equal(apiNode.getParent('.test'), undefined);
+		});
+	});
+
+	describe('APINode#getParents', function () {
+		it('should retunr array with all parents nodes', function () {
+			assert.deepEqual(apiNode.getParents(), [shadowTreeStub['.'], shadowTreeStub['.locations']]);
+		});
+	});
+
+	describe('APINode#setTree()', function () {
+		it('should add method #getTree(), that returns APIShadowTree instance from closure', function () {
+			assert.equal(apiNode.getTree(), shadowTreeStub);
+		});
+	});
+
+});
