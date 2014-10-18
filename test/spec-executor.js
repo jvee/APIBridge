e49e8fafdc4b015cbedae5eb64a93d62b3d2e341
Ivan Zasimoff <jvee99@gmail.com>
2014-10-18 20:53:40 +0400

    Тесты для H#extendFunctions() вынесены и отрефакторены из Executor#exec()

--- test/spec-executor.js
+++ test/spec-executor.js
@@ -154,11 +154,7 @@ describe('Executor', function () {
 	describe('#exec()', function () {
 
 		var serverConfig = require('./server/config'),
-			server, execRun, optionsChain, inc;
-
-		function incUp(x) {
-			inc += x.toString();
-		}
+			server, execRun, optionsChain;
 
 		before(function () {
 			server = require('./server/server').listen(serverConfig.port);
@@ -277,69 +273,6 @@ describe('Executor', function () {
 			}).fail(function () {
 				assert.equal(callbackExecuted, true);
 			});
-		});
-
-		it('should apply exend rules for options[prefilter|processResult]', function () {
-			function run(param) {
-				inc = '';
-
-				optionsChain[0][param] = [incUp.bind(null, 1), incUp.bind(null, 2)];
-				optionsChain[1][param] = [incUp.bind(null, 3)];
-				optionsChain[2][param] = incUp.bind(null, 4);
-
-				return execRun()
-					.then(function () {
-						optionsChain[0][param] = undefined;
-						optionsChain[1][param] = undefined;
-						optionsChain[2][param] = undefined;
-
-						assert.equal(inc, '1234');
-					});
-			}
-
-			return run('prefilter').then(run.bind(null, 'processResult'));
-		});
-
-		it('should apply extend rules for options[prefilter|processResult] after "null" in optionsChain', function () {
-			function run(param) {
-				inc = '';
-
-				optionsChain[0].prefilter = [incUp.bind(null, 1), incUp.bind(null, 2)];
-				optionsChain[1].prefilter = null;
-				optionsChain[2].prefilter = incUp.bind(null, 3);
-
-				return execRun()
-					.then(function () {
-						optionsChain[0][param] = undefined;
-						optionsChain[1][param] = undefined;
-						optionsChain[2][param] = undefined;
-
-						assert.equal(inc, '3');
-					});
-			}
-
-			return run('prefilter').then(run.bind(null, 'processResult'));
-		});
-
-		it('should apply extend rules for options[prefilter|processResult] after "null" in array in optionsChain"', function () {
-			function run(param) {
-				inc = '';
-
-				optionsChain[0].prefilter = [incUp.bind(null, 1), incUp.bind(null, 2)];
-				optionsChain[1].prefilter = [null, incUp.bind(null, 3)];
-				optionsChain[2].prefilter = incUp.bind(null, 4);
-
-				return execRun()
-					.then(function () {
-						optionsChain[0][param] = undefined;
-						optionsChain[1][param] = undefined;
-						optionsChain[2][param] = undefined;
-
-						assert.equal(inc, '34');
-					});
-			}
-
-			return run('prefilter').then(run.bind(null, 'processResult'));
 		});
 
 		// it('should exec promise.fail if options.prefilter returns false', function () {});
--- test/spec-helpers.js
+++ test/spec-helpers.js
@@ -30,4 +30,66 @@ describe('Helpers', function () {
 		});
 	});
 
+	describe("#extendFunctions()", function () {
+		var extendFunctions = H.extendFunctions,
+			optionsChain, extendFunctionSome, result;
+
+		var f1 = function () {},
+			f2 = function () {},
+			f3 = function () {},
+			f4 = function () {};
+
+		beforeEach(function () {
+			optionsChain = [{}, {}, {}];
+		});
+			
+		it('shoult return function as a factory', function () {
+			assert.equal(typeof extendFunctions(), 'function');
+		});
+
+		it('should apply extend rules for passed option of optionsChain', function () {
+			extendFunctionSome = extendFunctions('some');
+
+			optionsChain[0]['some'] = [f1, f2];
+			optionsChain[1]['some'] = [f3];
+			optionsChain[2]['some'] = f4;
+
+			result = extendFunctionSome(optionsChain[0], optionsChain[1], optionsChain[2]);
+
+			assert.equal(result.length, 4);
+			assert.equal(result[0], f1);
+			assert.equal(result[1], f2);
+			assert.equal(result[2], f3);
+			assert.equal(result[3], f4);
+		});
+
+		it('should apply extend rules for passed options after "null" in optionsChain ', function () {
+			extendFunctionSome = extendFunctions('some');
+
+			optionsChain[0]['some'] = [f1, f2];
+			optionsChain[1]['some'] = null;
+			optionsChain[2]['some'] = f3;
+
+			result = extendFunctionSome(optionsChain[0], optionsChain[1], optionsChain[2]);
+
+			assert.equal(result.length, 1);
+			assert.equal(result[0], f3);
+		});
+
+		it('should apply extend rules for passed options after "null" in array in optionsChain ', function () {
+			extendFunctionSome = extendFunctions('some');
+
+			optionsChain[0]['some'] = [f1, f2];
+			optionsChain[1]['some'] = [null, f3];
+			optionsChain[2]['some'] = f4;
+
+			result = extendFunctionSome(optionsChain[0], optionsChain[1], optionsChain[2]);
+
+			assert.equal(result.length, 2);
+			assert.equal(result[0], f3);
+			assert.equal(result[1], f4);
+		});
+
+	});
+
 });
