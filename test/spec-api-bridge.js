a9e8074ad6754c7b9a1ab03dc2022218982a9477
Ivan Zasimoff <jvee99@gmail.com>
2014-10-26 15:58:45 +0400

    Подготовлен головной модуль apiBridge и основа интеграционных тестов для него

---
+++ test/spec-api-bridge.js
@@ -0,0 +1,50 @@
+var assert = require('assert'),
+	serverConfig = require('./server/config'),
+	server = require('./server/server'),
+	apiBridge = require('../lib/api-bridge.js'),
+	testHost, apiDecl, api;
+
+describe('apiBridge integration test', function () {
+
+	before(function () {
+		server = server.listen(serverConfig.port);
+		testHost = 'http://' + serverConfig.host + ':' + serverConfig.port + '/';
+	});
+
+	after(function (done) {
+		server.close(done);
+	});
+
+	beforeEach(function () {
+		apiDecl = {
+			'.': {
+				name: 'TestAPI',
+				nodeType: 'root'
+			},
+
+			'.layer': {
+				name: 'layer',
+				nodeType: 'models'
+			},
+
+			'.layer.handlerOne': {
+				name: 'handlerOne',
+				nodeType: 'endpoints',
+				url: testHost + 'layer/handlerOne'
+			}
+		};
+
+		api = apiBridge(apiDecl);
+	});
+
+	it('should export correct strucutre', function () {
+
+		assert.ok(api);
+		assert.ok(api.layer);
+		assert.ok(api.layer.handlerOne);
+		assert.equal(typeof api.layer.handlerOne, 'function');
+
+	});
+
+
+});
