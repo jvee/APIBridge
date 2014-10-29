f6f725f63045a3007aaa363584147d8372f84c9a
Ivan Zasimoff <jvee99@gmail.com>
2014-10-30 00:51:21 +0400

    Интеграционные тесты для хэндлера (перенесены из Executor)

--- test/spec-api-bridge.js
+++ test/spec-api-bridge.js
@@ -17,12 +17,40 @@ describe('apiBridge integration test', function () {
 
 	beforeEach(function () {
 		apiDecl = {
-			'.': {},
+			'.': {
+				cascade: {
+					rootLevel: true
+				},
+				prefilter: function (options, response) {
+					assert.ok(options.cascade.rootLevel);
+					assert.ok(options.cascade.layerLevel);
+					assert.ok(options.cascade.handlerLevel);
+					assert.equal(this, api);
+				},
+				processResult: function (options, response) {
+					assert.ok(response.data);
+					// совсем не нравится "response.request"
+					assert.ok(response.request);
+					assert.equal(this, api);
 
-			'.layer': {},
+					// это не правильно? 
+					// хотя может и вариант, т.к это простой способ
+					// вынесте из объекта под объект
+					return response;
+				},
+			},
+
+			'.layer': {
+				cascade: {
+					layerLevel: true
+				}
+			},
 
 			'.layer.handlerOne': {
-				url: testHost + 'layer/handlerOne'
+				url: testHost + 'layer/handlerOne',
+				cascade: {
+					handlerLevel: true
+				}
 			}
 		};
 
@@ -30,11 +58,50 @@ describe('apiBridge integration test', function () {
 	});
 
 	it('should export correct strucutre', function () {
-
 		assert.ok(api);
 		assert.ok(api.layer);
 		assert.ok(api.layer.handlerOne);
-		assert.equal(typeof api.layer.handlerOne, 'function');
+	});
+
+	describe('API handler', function () {
+		var callbackExecuted;
+	
+		function callback(response) {
+			assert.equal(this, api);
+			callbackExecuted = true;
+		}
+
+		beforeEach(function () {
+			callbackExecuted = false;
+		});
+
+
+		it('should be a function', function () {
+			assert.equal(typeof api.layer.handlerOne, 'function');
+		});
+
+		it('should return promise', function () {
+			assert.equal(typeof api.layer.handlerOne().then, 'function');
+		});
+
+		it('should create request', function () {
+			return api.layer.handlerOne(callback)
+				.then(function (response) {
+					assert.equal(callbackExecuted, true);
+					assert.equal(response.request.status, 200);
+				});
+		});
+
+		it('should fail if executed with wrong params', function () {
+			return api.layer.handlerOne({ code: 404 }, callback)
+				.fail(function (response) {
+					assert.equal(callbackExecuted, true);
+					// не сохранена структура response при then и fail
+					assert.equal(response.status, 404);
+				});
+		});
+
+		// it('should exec promise.fail if options.prefilter returns false', function () {});
 
 	});
 
