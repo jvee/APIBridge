6db3ec5a48c1f05545d56950c93b0bd6c8b33a92
Ivan Zasimoff <jvee99@gmail.com>
2014-11-17 23:41:46 +0400

    Реализован нодовский transport

---
+++ test/helpers/spec-transport.js
@@ -0,0 +1,30 @@
+var assert = require('assert'),
+	serverConfig = require('../server/config'),
+	server = require('../server/server'),
+	transport = require('../../lib/helpers/transport.js');
+
+describe('helpres/transport', function () {
+	var testHost;
+
+	before(function () {
+		server = server.listen(serverConfig.port);
+		testHost = 'http://' + serverConfig.host + ':' + serverConfig.port + '/';
+	});
+
+	after(function (done) {
+		server.close(done);
+	});
+
+	it('should return promise', function () {
+		assert.equal(typeof transport({url: testHost}).then, 'function');
+	});
+
+	it('should accept type (request method) option', function () {
+		return transport({url: testHost, type: 'POST'})
+			.then(function (response) {
+				// возвращать в ответе сервера
+				assert.equal(response.request.method, 'POST');
+			});
+	});
+
+});
