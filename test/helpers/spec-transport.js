91a15dd2fe4152279616ff16d57851b60eb29341
Ivan Zasimoff <jvee99@gmail.com>
2014-11-24 23:09:10 +0300

    Небольшой рефакторинг + тесты для helpers.transport

--- test/helpers/spec-transport.js
+++ test/helpers/spec-transport.js
@@ -19,10 +19,40 @@ describe('helpres/transport', function () {
 		assert.equal(typeof transport({url: testHost}).then, 'function');
 	});
 
+	it('should return nothing if options.transport passed', function () {
+		assert.equal(typeof transport({transport: true}), 'undefined');
+	});
+
+	it('should fail if server responded with error status code', function () {
+		return transport({url: testHost, data: {code: 404}})
+			.fail(function (result) {
+				assert.ok(result.error);
+				assert.equal(result.status, 404);
+			});
+	});
+
+	it('should fail if requested unreachable host', function () {
+		return transport({url: 'http://localhost/unreachable'})
+			.fail(function (result) {
+				assert.ok(result.error);
+			});
+	});
+
+	it('should return correct result strucutre', function () {
+		return transport({url: testHost})
+			.then(function (result) {
+				assert.equal(result.status, 200);
+				assert.equal(typeof result.statusText, 'string');
+				assert.equal(typeof result.data, 'object');
+				assert.equal(typeof result.request, 'object');
+				assert.equal(typeof result.headers, 'object');
+			});
+	});
+
 	it('should accept type (request method) option', function () {
 		return transport({url: testHost, type: 'POST'})
-			.then(function (response) {
-				assert.equal(response.data.method, 'POST');
+			.then(function (result) {
+				assert.equal(result.data.method, 'POST');
 			});
 	});
 
