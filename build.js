ce00c7f46ae7ad1318c5b29813027e547c9b46a2
jvee <jvee99@gmail.com>
2015-06-25 14:59:22 +0300

    Переработана сборка

--- build.js
+++ build.js
@@ -1,40 +1,58 @@
+// TODO
+// 1. Выработать правила для написания модулей, плагинов, бандлов,
+// standalone-api и их последующей сборки
+// 2. Можно отказаться от requirejs в пользу других утилит
+// https://github.com/tobie/module-grapher
+// https://github.com/tobie/modulr-node
+// https://github.com/Magnetme/yoloader
+
 var requirejs = require('requirejs');
 
 var config = {
 		baseUrl: __dirname + '/lib',
-		name: 'model',
-		// в будузем exclude и path не понадобятся, временная хрень
-		exclude: ['node-jquery', 'jQuery', 'q', 'extend'],
+		name: 'rapier',
 		paths: {
-			'node-jquery': '../node_modules/node-jquery/index',
-			'jQuery': '../node_modules/node-jquery/node_modules/jQuery/lib/node-jquery',
-			'q': '../node_modules/q/q',
-			'extend': '../node_modules/extend/index'
+			'q': 'empty:',
+			'extend': 'empty:',
+			'control-najax': 'empty:'
 		},
 		out: './api.js',
 		optimize: 'none',
-		findNestedDependencies: true,
+		logLevel: 0,
 		cjsTranslate: true,
-		onModuleBundleComplete: function (data) {
-			var fs = require('fs'),
-			amdclean = require('amdclean'),
-			outputFile = data.path;
-
-			fs.writeFileSync(outputFile, amdclean.clean({
-				'filePath': outputFile,
-				'prefixMode': 'camelCase'
-			}));
-		}
+		onBuildWrite: cleanSrc
 	};
 
 requirejs.optimize(config, function (buildResponse) {
-	//buildResponse is just a text output of the modules
-	//included. Load the built file for the contents.
-	//Use config.out to get the optimized file contents.
-	var contents = fs.readFileSync(config.out, 'utf8');
-
-	console.log(arguments, contents);
-}, function(err) {
-	console.log(arguments);
-	//optimization err callback
+	console.log(buildResponse);
 });
+
+function cleanSrc(name, path, content) {
+  return content
+						// убираем все require()
+						// TODO: нужно все декларации переменных вынести в отдельный
+						// "var", без запятных в конце
+						.replace(/^.*require\(.*\).*$/mg, '')
+						// Уюираем все обращения к неймспейсу H
+						// TODO	нужно переосмыслить органицацию модуля хэлперов
+						.replace(/^.*\bH\b.*$/mg, '')
+						// Избавляемся от вхождений define(...)
+						// Вставляется автоматически r.js
+						// опция - cjsTranslate
+            .replace(/define\(.*\{/, '')
+						// Меняем module.exports на return, для единобразия,
+						// что бы потом удалить вместе с закрывающими скобками блока define
+						// TODO Важно, что бы module exports всегдф был последней строкой
+            .replace(/module\.exports\s*=\s*/, 'return ')
+						// Чистим модуль от return'а в никуда
+						// TODO
+						// 1. Проверить, что все модули возвращают задекларированный объект
+						// 2. в return нужно парсить имя модуля
+            .replace(/\s*return\s+[^\}]+(\}\s*?\);[^\w\}]*)$/, '\n$1')
+						// Удаляем закрывающие скобки define
+            .replace(/\}\);\s*$/, '')
+						// На всякий случай чистим пустые строки в начале файла
+						// TODO на этом месте можно добавлять разделить для модуля
+						// что бы потом проще воспринимались склееные файлы
+            .replace(/^\s*\n(\s*\w*)/, '$1');
+}
